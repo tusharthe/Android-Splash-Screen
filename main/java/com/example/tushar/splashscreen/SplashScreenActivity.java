ab852daa2022d946a4d630bcf125c33b5451a3b7
Tushar kanti <20502003+tusharthe@users.noreply.github.com>
2018-08-02 08:31:01 +0530

    app/src/main

---
+++ main/java/com/example/tushar/splashscreen/SplashScreenActivity.java
@@ -0,0 +1,48 @@
+package com.example.tushar.splashscreen;
+
+import android.content.Intent;
+import android.location.Location;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private int SLEEP_TIMER = 3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_splash_screen_activity);
+        LogoLauncher logoLauncher = new LogoLauncher();
+        logoLauncher.start();
+
+
+
+    }
+
+    private class LogoLauncher extends Thread{
+        public void run(){
+            try{
+                sleep(1000 * SLEEP_TIMER);
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+
+            Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+            startActivity(intent);
+            SplashScreenActivity.this.finish();
+        }
+    }
+
+
+
+}
